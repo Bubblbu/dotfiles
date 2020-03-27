9f156b47e6f9cd3e45e19780250d9a11c9850e83
bubblbu <asura.enkhbayar@gmail.com>
2020-03-26 21:00:47 -0700

    moved jl to python

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -12,5 +12,4 @@ alias smr="sh -c 'xrandr --output HDMI2 --off --output eDP1 --mode 1920x1080 --r
 
 # Utiities
 alias restart_wifi="sudo service network-manager restart"
-alias jl="jupyter lab"
 alias aptdate="sudo apt update && sudo apt upgrade"
