9059d395c26d6e802d82665ff4cdf9aadb056848
bubblbu <asura.enkhbayar@gmail.com>
2020-04-03 00:16:54 -0700

    add nbstrip alias for jupyter notebooks

--- python/aliases.zsh
+++ python/aliases.zsh
@@ -14,3 +14,12 @@ jk_install() {
 jk_remove() {
     jupyter kernelspec remove $1
 }
+
+# Strip jupyter notebooks
+alias nbstrip_jq="jq --indent 1 \
+    '(.cells[] | select(has(\"outputs\")) | .outputs) = []  \
+    | (.cells[] | select(has(\"execution_count\")) | .execution_count) = null  \
+    | .metadata = {\"language_info\": {\"name\": \"python\", \"pygments_lexer\": \"ipython3\"}} \
+    | .cells[].metadata = {} \
+    '"
+
