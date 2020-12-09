fe3b7a3c31e44461c9eb451e85102cb91979fe6b
bubblbu <asura.enkhbayar@gmail.com>
2020-12-08 20:48:39 -0800

    add snap applications to XDA_DATA_DIRS

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,2 +1,3 @@
 export PATH="./bin:/usr/local/bin:/usr/local/sbin:$ZSHDOT/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
+export XDG_DATA_DIRS="$XDG_DATA_DIRS:/var/lib/snapd/desktop/"
