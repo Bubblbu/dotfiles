4c3599cab22ed9451e4e4a9e7f6da7750d6ebc6c
bubblbu <asura.enkhbayar@gmail.com>
2021-08-30 20:35:03 -0700

    move prompt settings into correct place

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -1,19 +1,30 @@
 export ZSH=$HOME/.oh-my-zsh
 
-# plugins
-plugins=(git colored-man-pages zsh-autosuggestions)
+export TERM="xterm-256color"
+
+# Theme config
+ZSH_THEME="powerlevel10k/powerlevel10k"
+POWERLEVEL9K_MODE='nerdfont-complete'
+
+#POWERLEVEL9K_PROMPT_ON_NEWLINE=true
+POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(virtualenv dir vcs)
+POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(status root_indicator background_jobs history time)
+
+# Uncomment the following line to display red dots whilst waiting for completion.
+COMPLETION_WAITING_DOTS="true"
+
+# Plugins
+plugins=(git pip zsh-vi-mode zsh-completions z zsh-autosuggestions colored-man-pages)
+
+source $ZSH/oh-my-zsh.sh
 
 fpath=($ZSHDOT/functions $fpath)
 
 autoload -U $ZSHDOT/functions/*(:t)
 
-HISTFILE=~/.zsh_history
-HISTSIZE=10000
-SAVEHIST=10000
-
 setopt NO_BG_NICE # don't nice background tasks
 setopt NO_HUP
-setopt NO_LIST_BEEP
+setopt NO_HIST_BEEP
 setopt LOCAL_OPTIONS # allow functions to have local options
 setopt LOCAL_TRAPS # allow functions to have local traps
 setopt HIST_VERIFY
@@ -21,17 +32,12 @@ setopt SHARE_HISTORY # share history between sessions ???
 setopt EXTENDED_HISTORY # add timestamps to history
 setopt PROMPT_SUBST
 setopt CORRECT
-setopt COMPLETE_IN_WORD
-setopt IGNORE_EOF
 
 setopt APPEND_HISTORY # adds history
 setopt INC_APPEND_HISTORY SHARE_HISTORY  # adds history incrementally and share it across sessions
 setopt HIST_IGNORE_ALL_DUPS  # don't record dupes in history
 setopt HIST_REDUCE_BLANKS
 
-# don't expand aliases _before_ completion has finished
-#   like: git comm-[tab]
-setopt complete_aliases
 
 bindkey '^[^[[D' backward-word
 bindkey '^[^[[C' forward-word
