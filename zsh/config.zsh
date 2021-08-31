555354676dfb0a33cde4346c3881cf7df3dd5562
bubblbu <asura.enkhbayar@gmail.com>
2021-08-30 20:58:08 -0700

    remove p9k stuff

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -1,14 +1,7 @@
 export ZSH=$HOME/.oh-my-zsh
 
-export TERM="xterm-256color"
-
 # Theme config
 ZSH_THEME="powerlevel10k/powerlevel10k"
-POWERLEVEL9K_MODE='nerdfont-complete'
-
-#POWERLEVEL9K_PROMPT_ON_NEWLINE=true
-POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(virtualenv dir vcs)
-POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(status root_indicator background_jobs history time)
 
 # Uncomment the following line to display red dots whilst waiting for completion.
 COMPLETION_WAITING_DOTS="true"
