6b867cfb73bc4c29f3c3b4e7666bc745564b6df5
bubblbu <asura.enkhbayar@gmail.com>
2021-09-08 04:28:37 -0400

    minor changes. remove pacman call to install lsd

--- zsh/install.sh
+++ zsh/install.sh
@@ -14,6 +14,3 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 
 # Install zsh-vi-mode
 git clone https://github.com/jeffreytse/zsh-vi-mode ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-vi-mode
-
-# Install LSD
-pacman -S lsd
