2c9bb3b235d6d50a6357436d48d1cdcd36e74e3c
bubblbu <asura.enkhbayar@gmail.com>
2022-05-05 14:12:01 -0700

    remove old aliases for ubuntu/lubuntu & refactor aliases with env variables from zshrc

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -9,33 +9,20 @@ alias la='ls -a'
 alias lla='ls -la'
 alias lt='ls --tree'
 
-# Utiities
-alias aptdate="sudo apt update && sudo apt upgrade -y"
-alias restart_wifi="sudo service network-manager restart"
-alias restart_audo="pulseaudio -k && sudo alsa force-reload"
-
-# Clipboard
-alias pbcopy='xclip -selection clipboard'
-alias pbpaste='xclip -selection clipboard -o'
-
-# Monitor shortcuts
-alias smd="sh -c 'xrandr --output HDMI2 --auto --output eDP1 --auto --same-as HDMI2'"
-alias smr="sh -c 'xrandr --output HDMI2 --off --output eDP1 --auto --same-as HDMI2'"
-alias smright="sh -c 'xrandr --output HDMI2 --auto --output eDP1 --auto --left-of HDMI2'"
-alias smleft="sh -c 'xrandr --output HDMI2 --auto --output eDP1 --auto --right-of HDMI2'"
-alias smbelow="sh -c 'xrandr --output HDMI2 --auto --output eDP1 --auto --above HDMI2'"
-alias smtop="sh -c 'xrandr --output HDMI2 --auto --output eDP1 --auto --below HDMI2'"
-
 # Navigation
-alias cds="cd ~/Projects/scholcommlab"
-alias cdp="cd ~/Projects"
+alias cdp="cd "$PROJECTS""
+alias cddot="cd "$ZSHDOT""
 alias cdd="cd ~/Downloads"
-alias cddb="cd ~/Documents/Dropbox"
+alias cddb="cd ~/Dropbox/"
 
 alias ..='cd ..'
 alias ...='cd ...'
 alias ....='cd ....'
 alias .....='cd .....'
+
+# Clipboard
+alias pbcopy='xclip -selection clipboard'
+alias pbpaste='xclip -selection clipboard -o'
 
 # renaming tools
 alias zcp='zmv -C'
