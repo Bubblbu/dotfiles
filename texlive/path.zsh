95486d77b5cd1d9d4b04d9d60eb5b94284f9f152
bubblbu <asura.enkhbayar@gmail.com>
2022-05-05 19:40:08 -0700

    fix manpath and infopath

--- texlive/path.zsh
+++ texlive/path.zsh
@@ -1,4 +1,4 @@
 # Paths for TexLive
 export PATH=/usr/local/texlive/2019/bin/x86_64-linux:$PATH    
-export INFOPATH=$INFOPATH:/usr/local/texlive/2019/texmf-dist/doc/info
-export MANPATH=$MANPATH:/usr/local/texlive/2019/texmf-dist/doc/man
+export INFOPATH=/usr/local/texlive/2019/texmf-dist/doc/info:$INFOPATH
+export MANPATH=/usr/local/texlive/2019/texmf-dist/doc/man:$MANPATH
