1ed822235866c20989deb60e9508104afcd0d168
Asura <asura.enkhbayar@gmail.com>
2023-05-17 14:42:27 -0700

    update alias to git remove all deleted files

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,4 +1,4 @@
 # Other git aliases
 alias gl='git pull --prune'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
-alias grmdel='!git rm $(git ls-files --deleted)'
+alias grmall='!git rm $(git ls-files --deleted)'
